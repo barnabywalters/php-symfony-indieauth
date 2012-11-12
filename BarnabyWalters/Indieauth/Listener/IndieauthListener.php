5c4cee2d05c7c3ae191db226ca18ace460230db1
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-11-12 19:07:13 +0000

    Added docblocks to class and methods

--- BarnabyWalters/Indieauth/Listener/IndieauthListener.php
+++ BarnabyWalters/Indieauth/Listener/IndieauthListener.php
@@ -7,18 +7,48 @@ use Symfony\Component\HttpFoundation\Request,
 use BarnabyWalters\Indieauth\Token\IndieauthToken;
 
 /**
- * @todo add a check for ?token somewhere, not sure where is the most useful place though
+ * Indieauth Listener
+ * 
+ * This is a light extension of AbstractAuthenticationListener, which is where 
+ * the bulk of the logic goes on. Don’t be put off by the rather huge constructor,
+ * you get a *lot* for free.
+ * 
+ * @author Barnaby Walters http://waterpigs.co.uk <barnaby@waterpigs.co.uk>
  */
 class IndieauthListener extends AbstractAuthenticationListener {
     
+    /**
+     * Requires Authentication
+     * 
+     * An internal method used to tell whether or not a particular request
+     * requires authentication by indieauth. Specifically, it returns true if 
+     * the query has a 'token' query parameter.
+     * 
+     * This is quite prone to collision, so I raised an issue on the indieauth 
+     * software to support client-determined param name. If it is added, it will
+     * be specified here by a configurable property.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return bool Whether or not $request requires authentication
+     */
     public function requiresAuthentication(Request $request)
     {
         return $request->query->has('token');
     }
     
+    /**
+     * Attempt Authentication
+     * 
+     * An internal method called by it’s parent’s `handle()` method. Given a 
+     * request, tries to aithenticate it. Returns the authenticated token or 
+     * throws an exception (bubbled from `authenticationManager->authenticate()`)
+     * if something goes wrong.
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\Security\Authentication\TokenInterface The authenticated token
+     * @todo What to do if there is no token param? Should that conditional even be in here?
+     */
     public function attemptAuthentication(Request $request) {
-        // New user (not session or anon) — create an Indieauth Token
-        
         if ($this->requiresAuthentication($request)) {
             if ($this->logger !== null)
                 $this->logger->info('IndieauthListener found ?token param, so authenticating with indieauth');
