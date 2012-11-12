6f47ba58210fce13a2eb309c9292d1ee70907a4c
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-11-12 19:24:01 +0000

    Added docblocks

--- BarnabyWalters/Indieauth/Token/IndieauthToken.php
+++ BarnabyWalters/Indieauth/Token/IndieauthToken.php
@@ -3,12 +3,15 @@
 namespace BarnabyWalters\Indieauth\Token;
 
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * IndieauthToken
  * 
- * This is an extension of AnonymousToken with a constructor which only accepts
- * a single parameter, the token to use with the indieauth provider.
+ * This is an extremely light extension of UsernamePasswordToken (where most of 
+ * the business logic takes place). The only wrapping is in the constructor,
+ * which accepts at minimum a token (such as one returned by an indieauth 
+ * callback), and can optionally accept an array of roles and a user object.
  * 
  * @author Barnaby Walters http://waterpigs.co.uk <barnaby@waterpigs.co.uk>
  */
@@ -16,7 +19,10 @@ class IndieauthToken extends UsernamePasswordToken {
     
     /**
      * Construct
-     * @param string $token
+     * 
+     * @param string $token An indieauth token
+     * @param array $roles An array of roles to assign to the user
+     * @param string|UserInterface $user A user object to attach to the token
      */
     public function __construct($token, $roles=array('ROLE_USER'), $user = '') {
         parent::__construct($user, $token, 'rememberMeProviderKey', $roles);
