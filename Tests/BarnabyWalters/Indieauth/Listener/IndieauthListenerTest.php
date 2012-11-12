8900962a2196d8adfecbcd642b2fc337e29e14ec
Barnaby Walters <barnaby@waterpigs.co.uk>
2012-11-12 18:43:49 +0000

    Added phpdocs and phpunit config, Tests folder

---
+++ Tests/BarnabyWalters/Indieauth/Listener/IndieauthListenerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * IndieauthListenerTest
+ *
+ * @author Barnaby Walters
+ * @todo implement all these, and the other tests
+ */
+class IndieauthListenerTest extends PHPUnit_Framework_Testcase {
+    public function testRequiresAuthenticationForRequestsWithTokenQueryParam() {
+        
+    }
+}
+
+// EOF
